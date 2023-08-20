4501ac9e7b9810d4c404480455fe3898959ce306
Gowtham-Mathiyazhagan <gowthammathi07@gmail.com>
2023-08-20 17:20:04 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,27 @@
+// let nums=[4,5,2,1];
+// nums.sort((a,b)=>{
+//     return a-b;
+// })
+// let= queries = [3,10,21];
+// var answerQueries = function(nums, queries) {
+//    let start=0;
+//    let end=nums.length-1;
+//    let arr=[];
+//    while(start<=end)
+//    {
+//        let mid=(start+end)/2;
+//        let target;
+//     for(let i=0;i<queries.length;i++)
+//     {
+//         target=queries[i];
+//     }
+//     if(target>mid)
+//     {
+//         start=mid+1;
+//     }
+//     else{
+//         end=mid-1;
+//     }
+//     return arr.push(end);
+//    }
+// };  
